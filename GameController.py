9209a4d5b648f88ebbbfe07a93eae8d521bd6772
R-S-Johnson <riley.scott.johnson@gmail.com>
2024-12-13 13:29:04 -0500

    GameWindow Beginnings

--- GameController.py
+++ GameController.py
@@ -1,7 +1,8 @@
 from GameLogic import HnefataflBoard
+from GameWindow import GameWindow
 
 class GameController:
     def __init__(self):
         self.game = HnefataflBoard()
-        
+        self.window = GameWindow(self)
     
--- GameWindow.py
+++ GameWindow.py
@@ -3,18 +3,34 @@ import tkinter as tk
 class GameWindow(tk.Tk):
     def __init__(self, controller):
         super().__init__()
+        
+        # Controller for passing along user input
+        self.controller = controller
 
         # Constants
-        self.self.board_size = 11  # 11x11 board
-        self.self.square_size = 50  # Size of each square in pixels
-        self.colors = ("white", "gray")  # Alternating square colors
+        self.board_size = 11  # 11x11 board
+        self.square_size = 50  # Size of each square in pixels
 
         # Window
-        self.canvas = tk.Canvas(self, width=self.board_size * self.square_size, height=self.board_size * self.square_size)
+        self.canvas = tk.Canvas(self, width=self.board_size * self.square_size,
+                                height=self.board_size * self.square_size)
         self.canvas.pack()
+        
+        # Board and tags for click access
+        self.draw_board()
+        for row in range(self.board_size):
+            for col in range(self.board_size):
+                self.canvas.tag_bind(f"space_{row}_{col}", "<Button-1>", self.on_click)
+        
+        # Add Pieces
+        
 
-
-    def draw_board(self, board):
+    def draw_board(self):
+        """
+        Draws the game board at game start, tagging each cell
+        for Tkinter integration for easy cell click selection
+        """
+        colors = ("white", "gray")
         for row in range(self.board_size):
             for col in range(self.board_size):
                 color = self.colors[(row + col) % 2]
@@ -22,5 +38,19 @@ class GameWindow(tk.Tk):
                 y1 = row * self.square_size
                 x2 = x1 + self.square_size
                 y2 = y1 + self.square_size
-                self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="black")
+                self.canvas.create_rectangle(x1, y1, x2, y2,
+                                             fill=color, outline="black",
+                                             tags=f"space_{row}_{col}")
+                
 
+    def draw_piece(self, row, col, player):
+        """
+        Draw piece on board/canvas at location (row, col)
+        """
+        
+    def on_click(self, event):
+        """
+        Passes row, col to controller
+        to handle board interaction
+        """
+        
